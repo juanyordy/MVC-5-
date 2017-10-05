8e8c1b8d4c7638acbc5159212a04aeec3d3070d6
david226 <30418773+david226@users.noreply.github.com>
2017-10-05 16:36:51 -0500

    Añadi_Entidades

---
+++ MAntenimientoPersona/DataAccess/Entidades/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entidades
+{
+    public class Persona
+    {
+        public string nombre { set; get; }
+        public string direccion { set; get; }
+        public string correoElectronico { set; get; }
+        public string telefono { get; set; }
+    }
+}
---
+++ MAntenimientoPersona/DataAccess/Entidades/PersonaJuridica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entidades
+{
+    public class PersonaJuridica
+    {
+        public string ruc { set; get; }
+        public string urlPaginaWeb { set; get; }
+    }
+}
